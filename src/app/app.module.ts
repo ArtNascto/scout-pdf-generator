865dd1c5777323a2b15fb3189e7f4aeea2be1cd3
Arthur Nascimento <arthurgnascto@gmail.com>
2022-07-13 20:53:08 -0300

    WIP: add campos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatDividerModule } from '@angular/material/divider';
+import { PdfGeneratedComponent } from './components/pdf-generated/pdf-generated.component';
+import { NgSelectModule } from '@ng-select/ng-select';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatExpansionModule,
+    MatDialogModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    MatTableModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    MatDividerModule,
+    NgSelectModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, PdfGeneratedComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/pdf-generated/pdf-generated.component.ts
+++ src/app/components/pdf-generated/pdf-generated.component.ts
@@ -3,12 +3,11 @@ import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  templateUrl: './pdf-generated.component.html',
+  styleUrls: ['./pdf-generated.component.scss'],
 })
-export class AppComponent {
-  @ViewChild('invoice') invoiceElement!: ElementRef;
-  activitiesList: Array<string> = [
+export class PdfGeneratedComponent {
+  eventType: Array<string> = [
     'Acampamento',
     'Acantonamento',
     'Excursão',
@@ -77,43 +76,14 @@ export class AppComponent {
     'Trem',
     'Outros',
   ];
-  activities: Array<SelectDto> = [];
   sessions: Array<string> = [
     'Alcateia',
     'Tropa Escoteira',
     'Tropa Senior',
     'Clã Pioneiro',
   ];
-  selectedSession: any;
-  responsible: string = '';
-  selectedActivityType: any
+
   constructor() {}
 
-  ngOnInit(): void {
-
-  }
-  public generatePDF(): void {
-    html2canvas(this.invoiceElement.nativeElement, { scale: 3 }).then(
-      (canvas) => {
-        const imageGeneratedFromTemplate = canvas.toDataURL('image/png');
-        const fileWidth = 200;
-        const generatedImageHeight = (canvas.height * fileWidth) / canvas.width;
-        let PDF = new jsPDF('p', 'mm', 'a4');
-        PDF.addImage(
-          imageGeneratedFromTemplate,
-          'PNG',
-          0,
-          5,
-          fileWidth,
-          generatedImageHeight
-        );
-        PDF.html(this.invoiceElement.nativeElement.innerHTML);
-        PDF.save('solicitação.pdf');
-      }
-    );
-  }
-}
-export class SelectDto {
-  name: string = '';
-  value: string = '';
+  ngOnInit(): void {}
 }
